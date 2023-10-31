1ea1078691d05b9b9cb9d8dade855ff3e76e0c3e
Deepak Guptha <vzan2012@gmail.com>
2023-10-31 21:53:44 +0530

    feat: creating user page components

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,4 +1,7 @@
 import styles from "./styles.module.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function AboutLayout({ children }) {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,9 @@ export default function Home() {
   return (
     <>
       <main className="min-h-screen">
-        Hello !!! <br />
-        <br />
-        <br />
+        <h1>Home Page</h1>
         <Link href="/about">Link to About Page</Link>
+        <Link href="/users">Users</Link>{" "}
       </main>
     </>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
