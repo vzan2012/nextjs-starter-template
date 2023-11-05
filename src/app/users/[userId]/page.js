4abac1f41a5fc90862504856a3732e196ae30d7c
Deepak Guptha <vzan2012@gmail.com>
2023-11-05 21:59:08 +0530

    feat: generate dynamic metadata

--- src/app/users/[userId]/page.js
+++ src/app/users/[userId]/page.js
@@ -3,6 +3,16 @@ import getUser from "../../../../lib/getUser";
 import getUserPosts from "../../../../lib/getUserPosts";
 import UserPosts from "./components/UserPosts";
 
+export const generateMetadata = async ({ params }) => {
+  const userData = getUser(params.userId);
+  const user = await userData;
+
+  return {
+    title: `${user.firstname} ${user.lastname}`,
+    description: `This page is about the user - ${user.firstname} ${user.lastname}`,
+  };
+};
+
 const UserPage = async ({ params }) => {
   const userData = getUser(params.userId);
   const userPostsData = getUserPosts(params.userId);
