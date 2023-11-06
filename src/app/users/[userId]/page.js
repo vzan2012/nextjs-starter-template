2bd1951baaf63deb76135862ba56599e83621978
Deepak Guptha <vzan2012@gmail.com>
2023-11-06 21:31:07 +0530

    feat: Generate static params

--- src/app/users/[userId]/page.js
+++ src/app/users/[userId]/page.js
@@ -2,7 +2,9 @@ import { Suspense } from "react";
 import getUser from "../../../../lib/getUser";
 import getUserPosts from "../../../../lib/getUserPosts";
 import UserPosts from "./components/UserPosts";
+import getAllUsers from "../../../../lib/getAllUsers";
 
+// Generate Metadata
 export const generateMetadata = async ({ params }) => {
   const userData = getUser(params.userId);
   const user = await userData;
@@ -11,6 +13,14 @@ export const generateMetadata = async ({ params }) => {
     title: `${user.firstname} ${user.lastname}`,
     description: `This page is about the user - ${user.firstname} ${user.lastname}`,
   };
+};
+
+// Generate Static Params
+export const generateStaticParams = async () => {
+  const usersData = getAllUsers();
+  const users = await usersData;
+
+  return users.map((user) => ({ userId: user.id.toString() }));
 };
 
 const UserPage = async ({ params }) => {
